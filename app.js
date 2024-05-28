7dbb524ff52b72535a58c9b1aacea9605e22cf1f
raj14619 <raj14619@gmail.com>
2024-05-28 11:17:05 +0100

    Accidentally had deleted my db folder - unsure how I managed to do this. Had to put it back since jest was giving an error.

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
-const express = require ('express')
-
+const express = require('express');
 const app = express();
-
-//const db = require('/db');
-
-
-const { getTopics} = require('./controllers/topicsController')
+const { getTopics } = require('./controllers/topicsController');
+const db = require('./db'); // Import the database pool
 
 app.use(express.json());
 
-
-
 app.get('/api/topics', getTopics);
 
-
-app.use((err,req,res,next) => {
-    console.error(err.stack)
-    res.status(500).json({error: "internal server error"});
-})
-
-
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "internal server error" });
+});
 
 app.listen(4000, () => {
-    console.log("server running on port 4000");
-})
+  console.log("server running on port 4000");
+});
+
+module.exports = app; // Export the app for testing
--- models/topicsModel.js
+++ models/topicsModel.js
@@ -1,4 +1,4 @@
-const db = require ('../db/');
+const db = require ('../db');
 
 const fetchTopics = async () =>{
     const result = await db.query(`SELECT * FROM topics`);
