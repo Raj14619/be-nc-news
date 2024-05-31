7f12f6a0fe220ba9f26d2124e556278bc2666d8f
raj14619 <raj14619@gmail.com>
2024-05-31 14:26:44 +0100

    still trying to host - commented out code in listen.js

--- listen.js
+++ listen.js
@@ -4,26 +4,26 @@ const { PORT = 9090 } = process.env;
 //const runSeed = require('./db/seeds/run-seed.js')
 
 
-const devData = require('./db/data/development-data/index.js');
-const seed = require('./db/seeds/seed.js');
-const db = require('./db/connection.js');
+// const devData = require('./db/data/development-data/index.js');
+// const seed = require('./db/seeds/seed.js');
+// const db = require('./db/connection.js');
 
 
   
 
 
-// new changes made
-db.query('DROP DATABASE IF EXISTS nc_news_test;')
-db.query('DROP DATABASE IF EXISTS nc_news;')
-db.query('CREATE DATABASE nc_news_test;')
-db.query('CREATE DATABASE nc_news;')
+// // new changes made
+// db.query('DROP DATABASE IF EXISTS nc_news_test;')
+// db.query('DROP DATABASE IF EXISTS nc_news;')
+// db.query('CREATE DATABASE nc_news_test;')
+// db.query('CREATE DATABASE nc_news;')
 
-const runSeed = async () => {
-    await seed(devData);
-    return await db.end();
-  };
+// const runSeed = async () => {
+//     await seed(devData);
+//     return await db.end();
+//   };
   
-  runSeed();
+//   runSeed();
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
