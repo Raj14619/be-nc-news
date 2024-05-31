3a2adb2351871fc823408648c7df8ed6034a2b5b
raj14619 <raj14619@gmail.com>
2024-05-31 14:23:49 +0100

    still trying to host

--- listen.js
+++ listen.js
@@ -8,22 +8,22 @@ const devData = require('./db/data/development-data/index.js');
 const seed = require('./db/seeds/seed.js');
 const db = require('./db/connection.js');
 
+
+  
+
+
+// new changes made
+db.query('DROP DATABASE IF EXISTS nc_news_test;')
+db.query('DROP DATABASE IF EXISTS nc_news;')
+db.query('CREATE DATABASE nc_news_test;')
+db.query('CREATE DATABASE nc_news;')
+
 const runSeed = async () => {
     await seed(devData);
     return await db.end();
   };
   
   runSeed();
-  
-
-
-//new changes made
-// db.query('DROP DATABASE IF EXISTS nc_news_test;')
-// db.query('DROP DATABASE IF EXISTS nc_news;')
-// db.query('CREATE DATABASE nc_news_test;')
-// db.query('CREATE DATABASE nc_news;')
-
-
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
