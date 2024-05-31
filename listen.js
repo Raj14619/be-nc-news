c26d85b33167cbc49a3fe0e5b8e2e35ae58414b5
raj14619 <raj14619@gmail.com>
2024-05-31 14:15:51 +0100

    trying to host still

--- listen.js
+++ listen.js
@@ -4,6 +4,17 @@ const { PORT = 9090 } = process.env;
 //const runSeed = require('./db/seeds/run-seed.js')
 
 
+const devData = require('../data/development-data/index.js');
+const seed = require('./seed.js');
+const db = require('../connection.js');
+
+const runSeed = () => {
+    return seed(devData).then(() => db.end());
+  };
+  
+  runSeed();
+  
+
 
 //new changes made
 // db.query('DROP DATABASE IF EXISTS nc_news_test;')
