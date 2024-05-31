c4e18acea8a7a439e8cb0ce1bac23230591229f9
raj14619 <raj14619@gmail.com>
2024-05-31 11:41:01 +0100

    q12 done

--- controllers/articlesByArticleIdController.js
+++ controllers/articlesByArticleIdController.js
@@ -1,30 +1,22 @@
-const {fetchArticlesById} = require('../models/articlesByArticleIdModel')
+const { fetchArticlesById } = require('../models/articlesByArticleIdModel');
 
-const getArticlesById = async(req,res,next) =>{
+const getArticlesById = async (req, res, next) => {
+  let arr = Object.values(req.params);
+  let value = parseInt(arr[0], 10);
 
+  if (isNaN(value)) {
+    return res.status(400).send("You have entered an article_id which is not a number");
+  }
 
-
-    let arr = Object.values(req.params);
-
-    let value = parseInt(arr[0], 10)
-
-    //console.log(value)
-
-   if(isNaN(value)){
-    //console.log("Hi")
-    res.status(400).send("You have entered an article_id which is not a number")
-   }
-
-
-    // console.log(Object.values(req.params))
-
-    try{
-        const topics = await fetchArticlesById(value);
-        res.status(200).json(topics)
+  try {
+    const article = await fetchArticlesById(value);
+    if (!article) {
+      return res.status(404).json({ msg: 'Article not found' });
     }
-    catch (err){
-        next(err);
-    }
+    res.status(200).json({ article });
+  } catch (err) {
+    next(err);
+  }
 }
 
-module.exports = {getArticlesById}
+module.exports = { getArticlesById };
--- models/articlesByArticleIdModel.js
+++ models/articlesByArticleIdModel.js
@@ -1,11 +1,30 @@
-const db = require ('../db');
+const db = require('../db');
 
-const fetchArticlesById = async (input) =>{
-    const result = await db.query(`SELECT * FROM articles WHERE article_id = $1`, [input]);
+const fetchArticlesById = async (input) => {
+  const result = await db.query(`
+    SELECT 
+      article.article_id, 
+      article.title, 
+      article.topic, 
+      article.author, 
+      article.created_at, 
+      article.votes, 
+      article.article_img_url, 
+      COALESCE(comment.comment_count, 0) AS comment_count
+    FROM 
+      articles article
+    LEFT JOIN 
+      (SELECT article_id, COUNT(*) AS comment_count 
+       FROM comments 
+       WHERE article_id = $1
+       GROUP BY article_id) AS comment
+    ON 
+      article.article_id = comment.article_id
+    WHERE 
+      article.article_id = $1
+  `, [input]);
 
-   // console.log(result.rows)
-
-    return result.rows
+  return result.rows[0];
 }
 
-module.exports = {fetchArticlesById};
+module.exports = { fetchArticlesById };
